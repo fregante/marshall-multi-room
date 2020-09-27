99fd04b235538066131c8839793c97cb8805892e
Federico Brigante <opensource@bfred.it>
2020-09-27 01:45:09 -0500

    Improve API

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 const fetch = require('node-fetch');
 const { Parser } = require('xml2js');
-
 const keylogger = require('osx-keylogger');
 
 keylogger.listen((_, key) => {
 	switch (key) {
 		case '<69>':
 			volume = Math.min(10, volume + 1);
-			set(volume);
+			call('netremote.sys.audio.volume', volume);
 			break;
 		case '<68>':
-			volume--;
-			set(volume);
+			volume = Math.max(0, volume - 1);
+			call('netremote.sys.audio.volume', volume);
 			break;
 	}
 }, './keymap.json');
 
-async function set(value) {
-	const url = new URL('http://192.168.1.96/fsapi/SET/netremote.sys.audio.volume/');
+async function call(endpoint, value) {
+	const isSet = arguments.length > 1;
+	const url = new URL(`http://192.168.1.96/fsapi/${isSet ? 'SET' : 'GET'}/${endpoint}/`);
 	url.searchParams.set('pin', 1234);
 	url.searchParams.set('value', value);
 	const response = await fetch(url);
 	const xml = await response.text();
-}
-async function get() {
-	const url = new URL('http://192.168.1.96/fsapi/GET/netremote.sys.audio.volume/');
-	url.searchParams.set('pin', 1234);
-	const response = await fetch(url);
-	const xml = await response.text();
 	const { fsapiResponse } = await new Parser().parseStringPromise(xml);
-	return Number(fsapiResponse.value[0].u8[0]);
+	if (fsapiResponse.status[0] !== 'FS_OK') {
+		console.log(fsapiResponse);
+		throw new Error(fsapiResponse.status[0]);
+	}
+
+	const returned = fsapiResponse.value;
+	if (!isSet && returned && returned.length) {
+		if (returned[0].u8) {
+			return Number(returned[0].u8[0]);
+		}
+		console.log(returned);
+		return returned[0];
+	}
 }
-async function play() {
-	const url = new URL('http://192.168.1.96/fsapi/SET/netremote.play.control/');
-	url.searchParams.set('pin', 1234);
-	url.searchParams.set('value', 2);
-	const response = await fetch(url);
-	const xml = await response.text();
-	const { fsapiResponse } = await new Parser().parseStringPromise(xml);
-	console.log(fsapiResponse);
-}
+
 async function init() {
-	volume = await get();
+	volume = await call('netremote.sys.audio.volume');
 }
 
 init();
