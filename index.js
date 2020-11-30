9b365e90597f9a20955cf5e61695d5487c747e1d
Federico <me@fregante.com>
2020-11-29 20:19:00 -0600

    Improve volume management

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ function notifyError(error) {
 	notify(error.message, 'Error');
 }
 
+function updateVolume() {
+	volume = await call({ip}, 'sys.audio.volume');
+	setTimeout(updateVolume, 1000 * 3600); // Hourly
+}
+
 async function init() {
 	app.setActivationPolicy('accessory');
 	if (!app.isInApplicationsFolder()) {
@@ -27,11 +32,11 @@ async function init() {
 	}
 
 	const ip = await pTimeout(getIP(), 5000, 'No device could be found');
-	volume = await call({ip}, 'sys.audio.volume');
+	updateVolume();
 
 	await app.whenReady();
 	globalShortcut.register('F11', () => {
-		volume = Math.floor(Math.max(0, volume * 0.7));
+		volume = Math.floor(Math.max(0, volume - 1));
 		call({ip}, 'sys.audio.volume', volume).catch(notifyError);
 	});
 	globalShortcut.register('F12', () => {
